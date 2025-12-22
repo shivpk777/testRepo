b226e689f8d052299e030d56a0064a6b4129bef9
shivpk777 <Shivprasad.Konda@wissen.com>
2025-12-22 13:43:06 +0530

    second commit

--- A.java
+++ A.java
@@ -1,5 +1,7 @@
-public class A{
-public static void main(String args[]){
-System.out.println("hello");
+public class A
+{
+public static void main(String args[])
+{
+System.out.println("hello world");
 }
 }
