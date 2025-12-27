c477120c76d34dd66a7d51f9cd0abd2a3bfd0b8e
shivpk777 <Shivprasad.Konda@wissen.com>
2025-12-27 09:03:56 +0530

    next commit

--- Calculator.java
+++ Calculator.java
@@ -8,4 +8,12 @@ public class Calculator{
 		return a-b;
 	}
 
+	public int mul(int a,int b){
+		return a*b;
+	}
+
+	public int div(int a,int b){
+		return a/b;
+	}
+
 }
---
+++ CalculatorMain.java
@@ -0,0 +1,9 @@
+public class CalculatorMain{
+
+	public static void main(String args[]){
+	Calculator cal = new Calculator();
+	System.out.println("addition result is "+cal.add(1,3));
+	System.out.println("subtraction result is "+cal.sub(6,2));
+	}
+
+}
